58df35b3294bf01f7b17f828150caf5ac2b05115
TumharaGitHubUsername <TumharaGitHubEmail>
2025-11-18 16:16:38 +0530

    d

---
+++ .next/static/chunks/pages/analytics.js
@@ -0,0 +1,14 @@
+__turbopack_load_page_chunks__("/analytics", [
+  "static/chunks/node_modules_next_dist_compiled_next-devtools_index_82a36480.js",
+  "static/chunks/node_modules_next_dist_compiled_8a668f1c._.js",
+  "static/chunks/node_modules_next_dist_shared_lib_6a202fbd._.js",
+  "static/chunks/node_modules_next_dist_client_0b14ab40._.js",
+  "static/chunks/node_modules_next_dist_a3e9a08f._.js",
+  "static/chunks/node_modules_next_image_f00481cb.js",
+  "static/chunks/node_modules_react-dom_82bb97c6._.js",
+  "static/chunks/node_modules_recharts_es6_e53a36e2._.js",
+  "static/chunks/node_modules_5f02f62d._.js",
+  "static/chunks/[root-of-the-server]__84d717e3._.js",
+  "static/chunks/pages_analytics_index_5771e187.js",
+  "static/chunks/pages_analytics_index_a1c588bd.js"
+])
