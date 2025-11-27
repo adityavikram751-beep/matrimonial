7349b25edc713096f84f4ced7acfa71bf5e7f065
TumharaGitHubUsername <TumharaGitHubEmail>
2025-11-27 15:05:17 +0530

    ahhaaa

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -5,4 +5,41 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/node_modules_next_dist_compiled_edf9426d._.js",
       "static/chunks/node_modules_next_dist_shared_lib_b6d432bf._.js",
       "static/chunks/node_modules_next_dist_client_e70854a3._.js",
-      "static/chunks/node_mod
+      "static/chunks/node_modules_next_dist_76172dde._.js",
+      "static/chunks/node_modules_next_d42ab682._.js",
+      "static/chunks/node_modules_react-dom_82bb97c6._.js",
+      "static/chunks/node_modules_react-icons_37eda68c._.js",
+      "static/chunks/node_modules_4eee9ebb._.js",
+      "static/chunks/[root-of-the-server]__a524efff._.js",
+      "static/chunks/styles_globals_79636149.css",
+      "static/chunks/pages__app_5771e187._.js",
+      "static/chunks/pages__app_e4847761._.js"
+    ],
+    "/adminprofile": [
+      "static/chunks/[root-of-the-server]__ca33ece9._.js",
+      "static/chunks/node_modules_next_dist_compiled_next-devtools_index_82a36480.js",
+      "static/chunks/node_modules_next_dist_compiled_edf9426d._.js",
+      "static/chunks/node_modules_next_dist_shared_lib_6a202fbd._.js",
+      "static/chunks/node_modules_next_dist_client_0b14ab40._.js",
+      "static/chunks/node_modules_next_dist_a3e9a08f._.js",
+      "static/chunks/node_modules_next_image_f00481cb.js",
+      "static/chunks/node_modules_react-dom_82bb97c6._.js",
+      "static/chunks/node_modules_zod_v4_4d26c30f._.js",
+      "static/chunks/node_modules_react-icons_467e35c1._.js",
+      "static/chunks/node_modules_79d7066e._.js",
+      "static/chunks/pages_adminprofile_index_5771e187.js",
+      "static/chunks/pages_adminprofile_index_0a12376c.js"
+    ]
+  },
+  "devFiles": [],
+  "ampDevFiles": [],
+  "polyfillFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
