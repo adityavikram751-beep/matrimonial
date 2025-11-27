82a321bd365ba43402d363dfcb2ede73fafe70dc
TumharaGitHubUsername <TumharaGitHubEmail>
2025-11-27 15:04:26 +0530

    gggggg

---
+++ .next/server/middleware-build-manifest.js
@@ -0,0 +1,8 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": [
+      "static/chunks/node_modules_next_dist_compiled_next-devtools_index_82a36480.js",
+      "static/chunks/node_modules_next_dist_compiled_edf9426d._.js",
+      "static/chunks/node_modules_next_dist_shared_lib_b6d432bf._.js",
+      "static/chunks/node_modules_next_dist_client_e70854a3._.js",
+      "static/chunks/node_mod
